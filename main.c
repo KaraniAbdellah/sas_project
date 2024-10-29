de24a76d1b1fa793302f870d1a8f5002b95a0fbe
AbdellahKarani <abdellahkarani@gmail.com>
2024-10-29 12:33:07 +0100

    ajouter fonction de filtrage

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include "header.h"
-
+#include <time.h>
 
 /*
 	pour acceder a les champs avec un pointeur en utliser des fleches
@@ -12,7 +12,7 @@
 // Message
 void message() {
 	printf("\n-------------------------\n");
-	printf("Aucun Tache"); 
+	printf("Aucun Tache");
 	printf("\n-------------------------\n");
 }
 
@@ -30,6 +30,16 @@ void menu() {
 	
 }
 
+// Affiches Des Information sur la tache
+void affiche_info(Liste *tache) {
+	printf("titre: %s\n", tache->data.titre);
+	printf("description: %s\n", tache->data.description);
+	printf("status: %s\n", tache->data.status);
+	printf("priorite: %s\n", tache->data.priorite);
+	printf("Date: %d/%d/%d\n", tache->data.date.jour, tache->data.date.mois, tache->data.date.annes);
+	printf("-------------------------\n");
+}
+
 
 // Affiches Les Taches
 void afficher_taches(Liste *head) {
@@ -41,17 +51,40 @@ void afficher_taches(Liste *head) {
 	Liste *temp = head;
 	printf("\n-------------------------\n");
 	while (temp != NULL) {
-		printf("titre: %s\n", temp->data.titre);
-		printf("description: %s\n", temp->data.description);
-		printf("status: %s\n", temp->data.status);
-		printf("priorite: %s\n", temp->data.priorite);
-		printf("Date: %d/%d/%d\n", temp->data.date.jour, temp->data	.date.mois, temp->data.date.annes);
-		printf("-------------------------\n");
+		affiche_info(temp);
 		temp = temp->next;
 	}
 	
 }
 
+
+// Remplacer les spaces d'un string
+void remplacer_spaces(char *str) {
+	
+	for (int i = 0; str[i] != '\0'; i++) {
+		if (str[i] == ' ') str[i] = '_';
+	}
+	
+}
+
+// Ajouter les informations dans un fichier
+void ajouter_tache_fichier(Liste *tache) {
+	
+	FILE *p_file = fopen("tache.txt", "a");
+	if (p_file == NULL) {
+		printf("Error d'ajout a un fichier\n");
+		return;
+	}
+	
+	remplacer_spaces(tache->data.titre);
+	remplacer_spaces(tache->data.description);
+	remplacer_spaces(tache->data.status);
+	remplacer_spaces(tache->data.priorite);
+	
+	fprintf(p_file, "%s %s %s %s %d %d %d", tache->data.titre, tache->data.description,
+	tache->data.status, tache->data.priorite, tache->data.date.jour, tache->data.date.mois, tache->data.date.annes);
+	
+}
 
 // Ajouter Les Taches
 void ajouter_tache(Liste **head) {
@@ -65,23 +98,36 @@ void ajouter_tache(Liste **head) {
 	n_node->next = n_node->prev = NULL;
 	
 	printf("Enter le titre: ");
-	scanf("%[^\n]", n_node->data.titre);
+	scanf("%[^\n]", n_node->data.titre); n_node->data.titre[19] = '\0';
 	getchar();
 	printf("Enter la description: ");
-	scanf("%[^\n]", n_node->data.description);
+	scanf("%[^\n]", n_node->data.description); n_node->data.description[19] = '\0';
 	getchar();
 	printf("Enter le status: ");
-	scanf("%[^\n]", n_node->data.status);
+	scanf("%[^\n]", n_node->data.status); n_node->data.status[19] = '\0';
 	getchar();
 	printf("Enter le priorite: ");
-	scanf("%[^\n]", n_node->data.priorite);
+	scanf("%[^\n]", n_node->data.priorite); n_node->data.priorite[19] = '\0';
 	getchar();
 	printf("Enter la date (jour): ");
 	scanf("%d", &n_node->data.date.jour);
-	printf("Enter la date (moin): ");
-	scanf("%d", &n_node->data.date.mois);
-	printf("Enter la date (annes): ");
-	scanf("%d", &n_node->data.date.annes);
+
+	// get the year
+	time_t now = time(NULL);
+    struct tm *t = localtime(&now);
+    
+	int annes = t->tm_year + 1970;
+    int mois = t->tm_mon + 1;
+	
+	do {
+		printf("Enter la date (moin): ");
+		scanf("%d", &n_node->data.date.mois);
+	} while (n_node->data.date.mois < mois || n_node->data.date.mois <= 0);
+    
+	do {
+		printf("Enter la date (annes): ");
+		scanf("%d", &n_node->data.date.annes);
+	} while (n_node->data.date.annes < annes);
 	
 	if (*head == NULL) {
 		*head = n_node;
@@ -94,6 +140,9 @@ void ajouter_tache(Liste **head) {
 	}
 	n_node->prev = temp;
 	temp->next = n_node;
+	
+	// Ajouter la tache a un fichier
+	ajouter_tache_fichier(n_node);
 	
 }
 
@@ -162,10 +211,8 @@ void modifier_tache(Liste **head) {
 			printf("Enterz le neuveau jour: "); scanf("%d", &tache_modfier->data.date.jour);
 			printf("Enterz le neuveau moin: "); scanf("%d", &tache_modfier->data.date.mois);
 			printf("Enterz le neuveau annes: "); scanf("%d", &tache_modfier->data.date.annes);
-		} else if (choix_mod != 6) {
-			printf("Veillez Saiser Votre Operation\n");
 		} else {
-			
+			continue;
 		}
 	} while(choix_mod != 6);
 		
@@ -214,38 +261,104 @@ void supprimer_tache(Liste **head) {
 void filtrage_titre(Liste *head) {
 	
 	char *titre = (char *) malloc(sizeof(char) * 20);
-	printf("Enter le titre");
+	printf("Enter le titre: ");
 	scanf("%[^\n]s", titre); titre[19] = '\0';
 	
 	Liste *temp = head;
+	int check = 0;
+	printf("-------------------------\n");
 	while(temp != NULL) {
-		if (strcmp(temp->titre, titre) == 0) {
-			printf("titre: %s\n", temp->data.titre);
-			printf("description: %s\n", temp->data.description);
-			printf("status: %s\n", temp->data.status);
-			printf("priorite: %s\n", temp->data.priorite);
-			printf("Date: %d/%d/%d\n", temp->data.date.jour, temp->data	.date.mois, temp->data.date.annes);
-			printf("-------------------------\n");
+		if (strcmp(temp->data.titre, titre) == 0) {
+			check = 1;
+			affiche_info(temp);
 		}
-		
 		temp = temp->next;
 	}
+	if (check == 0) {
+		message();
+	}	
+	printf("\n");
 	
 }
 
 // filtrage des information par le status
 void filtrage_status(Liste *head) {
+
+	char *status = (char *) malloc(sizeof(char) * 20);
+	printf("Enter le status: ");
+	scanf("%[^\n]s", status); status[19] = '\0';
 	
-	printf("Not Implemented yet\n");
+	Liste *temp = head;
+	int check = 0;
+	printf("-------------------------\n");
+	while(temp != NULL) {
+		if (strcmp(temp->data.status, status) == 0) {
+			affiche_info(temp);
+		}
+		temp = temp->next;
+	}
+	if (check == 0) {
+		message();
+	}	
+	printf("\n");
+	
 	
 }
 
 // filtrage des information par le priorite
 void filtrage_priorite(Liste *head) {
 	
-	printf("Not Implemented yet\n");
+	char *priorite = (char *) malloc(sizeof(char) * 20);
+	printf("Enter le priorite: ");
+	scanf("%[^\n]s", priorite); priorite[19] = '\0';
+	
+	Liste *temp = head;
+	int check = 0;
+	printf("-------------------------\n");
+	while(temp != NULL) {
+		if (strcmp(temp->data.priorite, priorite) == 0) {
+			check = 1;
+			affiche_info(temp);
+		}
+		temp = temp->next;
+	}
+	if (check == 0) {
+		message();
+	}	
+	printf("\n");
 	
 }
+
+// filtrage des information par la date
+void filtrage_date(Liste *head) {
+	
+	int jour;
+	int mois;
+	int annes;
+	printf("Enter Le jour: ");
+	scanf("%d", &jour);
+	printf("Enter Le mois: ");
+	scanf("%d", &mois);
+	printf("Enter Le annes: ");
+	scanf("%d", &annes);
+	
+	Liste *temp = head;
+	int check = 0;
+	printf("-------------------------\n");
+	while(temp != NULL) {
+		if (temp->data.date.jour == jour && temp->data.date.mois == mois && temp->data.date.annes == annes) {
+			check = 1;
+			affiche_info(temp);
+		}
+		temp = temp->next;
+	}
+	if (check == 0) {
+		message();
+	}
+	printf("\n");
+	
+}
+
 
 // Menu de filtrage
 void menu_filtrage() {
@@ -253,7 +366,8 @@ void menu_filtrage() {
 	printf("1. titre \n");
 	printf("2. status\n");
 	printf("3. priorite\n");
-	printf("4. Quitter Session de Filtrage\n");
+	printf("4. date\n");
+	printf("5. Quitter Session de Filtrage\n");
 	
 }
 
@@ -269,6 +383,7 @@ void  filtrer_taches(Liste *head) {
 		menu_filtrage();
 		printf("Enter votre choix: ");
 		scanf("%d", &choix);
+		getchar();
 		switch(choix) {
 			case 1: {
 				filtrage_titre(head);
@@ -282,9 +397,13 @@ void  filtrer_taches(Liste *head) {
 				filtrage_priorite(head);
 				break;
 			}
+			case 4: {
+				filtrage_date(head);
+				break;
+			}
 			default: break;
 		}
-	} while(choix != 4);
+	} while(choix != 5);
 	
 }
 
@@ -316,6 +435,31 @@ int main() {
 		
 	return 0;
 }
+
+
+
+
+
+
+// explination 
+/*
+	// gets the current time in seconds since January 1, 1970
+	time_t now = time(NULL);
+	// line converts the time_t value now into a tm structure
+    struct tm *t = localtime(&now);
+    int year = t->tm_year + 1900;
+    // on ajouter 1900 car tm_year reprsent annes depuis 1900
+    // tm_year countr from 1900
+*/
+
+
+
+// Custom the date condition & storage in file
+
+
+
+
+
 
 
 
